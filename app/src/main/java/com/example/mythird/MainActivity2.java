4671fa6879529cb43ac3881186c108c521688928
TheCodeMonster <75868135+thecodemonster1@users.noreply.github.com>
2023-04-20 07:13:27 +0530

    delete the result button... all the variables are inside the onResume() method.. now all the buttons are containing displaying result while mathametical operations at the same time... unnecessory things are commented...

--- app/src/main/java/com/example/mythird/MainActivity2.java
+++ app/src/main/java/com/example/mythird/MainActivity2.java
@@ -15,12 +15,11 @@ public class MainActivity2 extends AppCompatActivity {
     private Button sub;
     private Button mul;
     private Button div;
-    private Button result;
     private TextView num1txt;
     private TextView num2txt;
     private TextView answer;
-    private static int new_num1;
-    private static int new_num2;
+//    private static int new_num1;
+//    private static int new_num2;
     private static int finalAns;
 
 
@@ -35,17 +34,16 @@ public class MainActivity2 extends AppCompatActivity {
         sub = findViewById(R.id.sub);
         mul = findViewById(R.id.mul);
         div = findViewById(R.id.div);
-        result = findViewById(R.id.result);
 
-        Intent intent = getIntent();
-        int new_num1 = intent.getIntExtra(MainActivity.number1, 0);
-        int new_num2 = intent.getIntExtra(MainActivity.number2, 0);
-
-        this.new_num1 = new_num1;
-        this.new_num2 = new_num2;
-
-        num1txt.setText(Integer.toString(new_num1));
-        num2txt.setText(Integer.toString(new_num2));
+//        Intent intent = getIntent();
+//        int new_num1 = intent.getIntExtra(MainActivity.number1, 0);
+//        int new_num2 = intent.getIntExtra(MainActivity.number2, 0);
+//
+//        this.new_num1 = new_num1;
+//        this.new_num2 = new_num2;
+//
+//        num1txt.setText(Integer.toString(new_num1));
+//        num2txt.setText(Integer.toString(new_num2));
 
     }
 
@@ -53,12 +51,23 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onResume(){
         super.onResume();
 
+        Intent intent = getIntent();
+        int new_num1 = intent.getIntExtra(MainActivity.number1, 0);
+        int new_num2 = intent.getIntExtra(MainActivity.number2, 0);
+
+//        this.new_num1 = new_num1;
+//        this.new_num2 = new_num2;
+
+        num1txt.setText(Integer.toString(new_num1));
+        num2txt.setText(Integer.toString(new_num2));
+
         answer = findViewById(R.id.Answer);
         add.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 Toast.makeText(MainActivity2.this, "+ Button is clicked!", Toast.LENGTH_SHORT).show();
                 finalAns = new_num1+ new_num2;
+                answer.setText(""+ finalAns);
             }
         });
 
@@ -67,6 +76,7 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View view){
                 Toast.makeText(MainActivity2.this, "- Button is clicked!", Toast.LENGTH_SHORT).show();
                 finalAns = new_num1 - new_num2;
+                answer.setText(""+ finalAns);
             }
         });
 
@@ -75,6 +85,7 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View view){
                 Toast.makeText(MainActivity2.this, "* Button is clicked!", Toast.LENGTH_SHORT).show();
                 finalAns = new_num1 * new_num2;
+                answer.setText(""+ finalAns);
             }
         });
 
@@ -83,16 +94,9 @@ public class MainActivity2 extends AppCompatActivity {
             public void onClick(View view){
                 Toast.makeText(MainActivity2.this, "/ Button is clicked!", Toast.LENGTH_SHORT).show();
                 finalAns = new_num1 / new_num2;
-            }
-        });
-
-        result.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                // Define code to perform when button is clicked
-//                Toast.makeText(MainActivity2.this, "= Button is clicked!", Toast.LENGTH_SHORT).show();
                 answer.setText(""+ finalAns);
             }
         });
+
     }
 }
